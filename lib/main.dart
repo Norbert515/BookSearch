261b95b4d9dad8a4f5e2a93a9d34b5ca19cce501
Norbert <norbertkozsir@gmail.com>
2018-02-10 14:06:42 +0100

    deleted comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:rxdart/rxdart.dart';
-import 'dart:async';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.green,
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,15 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,13 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final subject = new PublishSubject<String>();
 
   bool _isLoading = false;
-
-
-  void _floatingClick() {
-    setState(() {
-      //_items.add(_controller.text);
-    });
-  }
 
   void _textChanged(String text) {
     _isLoading = true;
@@ -85,9 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
   void _printBook(dynamic book) {
-    //print(book["volumeInfo"]["title"]);
-    print(book["volumeInfo"]["imageLinks"]["smallThumbnail"]);
-    print(book);
     setState(() {
       _items.add(new Book(book["volumeInfo"]["title"], book["volumeInfo"]["imageLinks"]["smallThumbnail"]));
     });
@@ -103,13 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       body: new Container(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         padding: new EdgeInsets.all(8.0),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -147,11 +114,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _floatingClick,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

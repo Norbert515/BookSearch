5c446b53b527dea4ef88e0b85de7f6b08b3579e7
Norbert <norbertkozsir@gmail.com>
2018-02-19 15:43:32 +0100

    removed unused print()s

--- lib/database.dart
+++ lib/database.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:test_app/model/Book.dart';
-
 
 
 class BookDatabase {
@@ -13,16 +11,11 @@ class BookDatabase {
 
   final String tableName = "Books";
 
-
   Database db;
-
-
 
   factory BookDatabase() {
     return _bookDatabase;
   }
-
-
 
   Future<bool> getBookStarStatus(Book book) async{
     var result = await db.rawQuery('SELECT star FROM $tableName WHERE id = "${book.id}"');
@@ -31,22 +24,16 @@ class BookDatabase {
     return result[0]["star"] == 1? true: false;
   }
 
-
-
   Future updateBookStarStatus(Book book) async {
     await db.inTransaction(() async {
       int id1 = await db.rawInsert(
           'INSERT OR REPLACE INTO $tableName(id, title, url, star) VALUES("${book.id}", "${book.title}", "${book.url}", ${book.starred? 1:0})');
-      print("inserted1: $id1");
     });
   }
-
-
 
   Future<String> getBookNotes(Book book) async{
     var result = await db.rawQuery('SELECT notes FROM $tableName WHERE id = "${book.id}"');
     if(result.length == 0)return "";
-    print(result[0]);
     return result[0]["notes"];
   }
 
@@ -56,11 +43,8 @@ class BookDatabase {
     await db.inTransaction(() async {
       int id1 = await db.rawInsert(
           'INSERT OR REPLACE INTO $tableName(id, title, url, star, notes) VALUES("${book.id}", "${book.title}", "${book.url}", ${book.starred? 1:0}, "${book.notes}")');
-      print("inserted1: $id1");
     });
   }
-
-
 
   BookDatabase._internal() {
     start();
@@ -71,11 +55,8 @@ class BookDatabase {
     // Get a location using path_provider
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, "demo.db");
-    print(path);
-
     db = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
-
           // When creating the db, create the table
           await db.execute(
               "CREATE TABLE $tableName (id STRING PRIMARY KEY, title TEXT, url TEXT, star BIT, notes TEXT)");

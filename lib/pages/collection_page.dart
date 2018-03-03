3dbf9f92c952f1bdcedf5828959bd10f3cd5825e
Norbert <norbertkozsir@gmail.com>
2018-03-03 18:08:27 +0100

    removed replaced BookDatabase with Repository on the NotesPage

---
+++ lib/pages/collection_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class CollectionPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _CollectionPageState();
+
+}
+
+
+class _CollectionPageState extends State<CollectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
--- lib/widgets/BookCard.dart
+++ lib/widgets/BookCard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-import 'package:test_app/database.dart';
 import 'package:test_app/model/Book.dart';
 
 
@@ -26,22 +25,7 @@ class BookCard extends StatefulWidget {
 
 class BookCardState extends State<BookCard> {
 
-  Book bookState;
 
-
-  @override
-  void initState() {
-    super.initState();
-    bookState = widget.book;
-    BookDatabase.get().getBook(widget.book.id)
-        .then((book){
-      if (book == null) return;
-      setState((){
-        bookState = book;
-      });
-    });
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +38,10 @@ class BookCardState extends State<BookCard> {
                 padding: new EdgeInsets.all(8.0),
                 child: new Row(
                   children: <Widget>[
-                    bookState.url != null?
+                    widget.book.url != null?
                     new Hero(
-                      child: new Image.network(bookState.url),
-                      tag: bookState.id,
+                      child: new Image.network(widget.book.url),
+                      tag: widget.book.id,
                     ):
                     new Container(),
                     new Expanded(
@@ -65,14 +49,14 @@ class BookCardState extends State<BookCard> {
                         children: <Widget>[
                           new Align(
                             child: new Padding(
-                              child: new Text(bookState.title, maxLines: 10),
+                              child: new Text(widget.book.title + "    " + widget.book.notes, maxLines: 10),
                               padding: new EdgeInsets.all(8.0),
                             ),
                             alignment: Alignment.center,
                           ),
                           new Align(
                             child: new IconButton(
-                              icon: bookState.starred? new Icon(Icons.star): new Icon(Icons.star_border),
+                              icon: widget.book.starred? new Icon(Icons.star): new Icon(Icons.star_border),
                               color: Colors.black,
                               onPressed: widget.onStarClick,
                             ),

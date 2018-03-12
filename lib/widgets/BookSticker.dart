5489673a551bd4280ad132a53fb3124674d05974
Norbert <norbertkozsir@gmail.com>
2018-03-12 22:21:04 +0100

    testing stamps

--- lib/widgets/BookSticker.dart
+++ lib/widgets/BookSticker.dart
@@ -32,6 +32,44 @@ class _BookStickerState extends State<BookSticker> with SingleTickerProviderStat
   @override
   Widget build(BuildContext context) {
 
+    var size = 90.0;
+
+    var card_width = 300.0 - size;
+    var card_height = 380.0 - size;
+
+    var inner_width = 230.0 - size;
+    var inner_height = 300.0 - size;
+
+    return new Material(
+      elevation: 8.0,
+      color: Colors.transparent,
+      child: new SizedBox(
+        width: card_width,
+        height: card_height,
+        child: new Stack(
+          children: <Widget>[
+            new Image.asset("assets/stamp_bg.png", width: card_width, height: card_height,),
+            new Align(
+              alignment: Alignment.center,
+              child: new Container(
+                width: inner_width + 30.0,
+                height: inner_height + 30.0,
+               // color: new Color.fromARGB(255, 206, 145, 54),
+                color: Colors.black26,
+              ),
+            ),
+         //   new Align(alignment: Alignment.center,child: new Image.asset("assets/s.png", width: 250.0, height: 300.0,)),
+            new Align(
+                alignment: Alignment.center,
+                child: new Image.asset("assets/test_img.jpg", width: inner_width, height: inner_height, fit: BoxFit.cover,)
+            ),
+
+          ],
+        ),
+      ),
+    );
+
+
     return new AnimatedBuilder(
         animation: animation,
         builder: (BuildContext context, Widget child) {

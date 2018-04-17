e1dc469e4ca299978bceb229a48a95bed1153ec3
Norbert <norbertkozsir@gmail.com>
2018-04-17 15:09:14 +0200

    removed delay attribute

--- lib/utils/index_offset_curve.dart
+++ lib/utils/index_offset_curve.dart
@@ -6,10 +6,9 @@ import 'package:flutter/material.dart';
 class IndexOffsetCurve extends Curve {
 
 
-  IndexOffsetCurve(this.index, {this.delay = 1.0});
+  IndexOffsetCurve(this.index);
 
   final int index;
-  final double delay;
 
   @override
   double transform(double t) {
